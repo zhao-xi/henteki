f2b293f60199ff9e3efb4ad56b43fe3d0c781313
Ubuntu <ubuntu@ip-172-31-40-58.ap-northeast-1.compute.internal>
2019-10-10 12:13:50 +0000

    added commenting

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+  before_action :set_post, only: [:create, :destroy]
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @post, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.new(comment_params)
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @post, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params.require(:comment).permit(:name, :text, :post_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191010101001) do
+ActiveRecord::Schema.define(version: 20191010114341) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "text"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "japanese"
